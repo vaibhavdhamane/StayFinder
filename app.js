187378e9c321a12d335240706723abbdc64d45c3
vaibhavdhamane <vaibhavdhamane00@gmail.com>
2026-08-13 02:34:27 +0530

    cloud error fix

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-if(process.env.NODE_ENV != "production"){
+// if(process.env.NODE_ENV != "production"){
+// require("dotenv").config();
+// }
 require("dotenv").config();
-}
 
 const express = require("express");
 const app = express();
