44fe4a4705e1fd2e5769c9b8ad2e5552880b51ae
vaibhavdhamane <vaibhavdhamane00@gmail.com>
2026-08-13 13:52:32 +0530

    Remove temporary Cloudinary debug log

--- CloudConfig.js
+++ CloudConfig.js
@@ -1,12 +1,6 @@
 const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
-console.log("Cloudinary config check:", {
-    cloud_name: process.env.CLOUD_NAME,
-    api_key_exists: !!process.env.CLOUD_API_KEY,
-    api_secret_exists: !!process.env.CLOUD_API_SECRET
-});
-
 cloudinary.config({
     cloud_name : process.env.CLOUD_NAME,
     api_key : process.env.CLOUD_API_KEY,
